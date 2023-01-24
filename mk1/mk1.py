ef0427b2568350f78a47e659277c0951df9eef7e
ethannussinov <89862811+ethannussinov@users.noreply.github.com>
2023-01-24 13:14:30 -0600

    hi

--- mk1/mk1.py
+++ mk1/mk1.py
@@ -1,1 +1,21 @@
-h = ('hi im ethan')
+import pymatgen.core.structure
+from pymatgen.io.ase import AseAtomsAdaptor
+from ase.visualize import view
+from pymatgen.io.cif import CifParser
+
+
+
+blah = pymatgen.core.Structure.from_file('mk1/testPOSCAR.txt')
+# Supported formats include CIF, POSCAR/CONTCAR, CHGCAR, LOCPOT, 
+# vasprun.xml, CSSR, Netcdf and pymatgen’s JSON-serialized structures.
+print(blah)
+
+
+
+hi = AseAtomsAdaptor.get_atoms(blah)
+
+view(hi)
+
+
+
+
