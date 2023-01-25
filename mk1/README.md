6870e22ee6ecf7b002a061a7cc3317e79f8f4b1c
ethannussinov <89862811+ethannussinov@users.noreply.github.com>
2023-01-24 21:23:11 -0600

    hi

--- mk1/README.md
+++ mk1/README.md
@@ -1,4 +1,4 @@
-The goal for mk1 is to read in a crystal structure file (CSF) from various databases (IMSE, OQMD, AFLOW, ICSD, etc). into pymatgen. From pymatgen, we will use the pymatgen.io.ase module to pipeline the CSF to Atomic Simulation Environment. Using the view module from ASE, we will print the crystal structure and test it against a printed VESTA crystal structure of the same variety.
+The goal for mk1 is to read in a crystal structure file (CSF) from various databases (MP, OQMD, AFLOW, ICSD, etc). into pymatgen. From pymatgen, we will use the pymatgen.io.ase module to pipeline the CSF to Atomic Simulation Environment. Using the view module from ASE, we will print the crystal structure and test it against a printed VESTA crystal structure of the same variety.
 
 Working backwards, the first test case we will aim to fulfill is to match the ASE CSF with the VESTA CSF.
 
--- mk1/mk1.py
+++ mk1/mk1.py
@@ -3,20 +3,22 @@ from pymatgen.io.ase import AseAtomsAdaptor
 from ase.visualize import view
 from pymatgen.io.cif import CifParser
 
-from mp_api.client import MPRester
-with MPRester(api_key="C4VoKwbKXbVC9dk85Uq5G5tig1LWbPKu") as mpr:
-    data = mpr.materials.get_data_by_id("mp-4019")
+# from mp_api.client import MPRester
+# with MPRester(api_key="C4VoKwbKXbVC9dk85Uq5G5tig1LWbPKu") as mpr:
+#     data = mpr.materials.get_data_by_id("mp-4019")
 
-class Test:
+class crystalClass:
     def file_readin(filething):
+        # if filething.endswith('.cif' or '.txt' or '.POSCAR'):
+    
         structure  = pymatgen.core.Structure.from_file(filething)
         # Supported formats include CIF, POSCAR/CONTCAR, CHGCAR, LOCPOT, 
         # vasprun.xml, CSSR, Netcdf and pymatgen’s JSON-serialized structures.
 
         visualized = AseAtomsAdaptor.get_atoms(structure)
         return(view(visualized))
-    # file_readin(data)
+    file_readin('mk1/crystal_files/OQMD_CaTiO3_POSCAR.txt')
 
-print(data)
+# print(data)
 
 
