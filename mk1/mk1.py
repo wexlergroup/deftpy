afc71e4b4d3af369818987b0e5fd7fe1159a4fb8
ethannussinov <89862811+ethannussinov@users.noreply.github.com>
2023-01-25 20:29:05 -0600

    test

--- mk1/mk1.py
+++ mk1/mk1.py
@@ -2,24 +2,20 @@ import pymatgen.core.structure
 from pymatgen.io.ase import AseAtomsAdaptor
 from ase.visualize import view
 from pymatgen.io.cif import CifParser
+from mp_api.client import MPRester
 
-# from mp_api.client import MPRester
-# with MPRester(api_key="C4VoKwbKXbVC9dk85Uq5G5tig1LWbPKu") as mpr:
+# with MPRester(api_key="c") as mpr:
 #     data = mpr.materials.get_data_by_id("mp-4019")
 
 class crystalClass:
     def file_readin(filething):
         if filething.startswith('mp-'):
-            with MPRester(api_key="something") as mpr:
+            with MPRester(api_key="C4VoKwbKXbVC9dk85Uq5G5tig1LWbPKu") as mpr:
                 data = mpr.materials.get_data_by_id(filething)
 
         else:
             structure = pymatgen.core.Structure.from_file(filething)
             visualized = AseAtomsAdaptor.get_atoms(structure)
-            # Supported formats include CIF, POSCAR/CONTCAR, CHGCAR, LOCPOT, 
-            # vasprun.xml, CSSR, Netcdf and pymatgen’s JSON-serialized structures.
+            # works with cif, vasp, and poscar files
         return(view(visualized))
-    file_readin('mk1/crystal_files/OQMD_CaTiO3_POSCAR.txt')
-
-
-
+    file_readin("mp-4019")
