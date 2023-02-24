f60cd06571124845749a8bf08e54d24d92ebf10d
Ethan <ethannussinov@gmail.com>
2023-02-24 13:02:21 -0600

    ji

--- mk1/testttest.py
+++ mk1/testttest.py
@@ -9,7 +9,41 @@ import pandas as pd
 from pymatgen.core.composition import Composition
 
 
-structure = pymatgen.core.Structure.from_file('mk1/crystal_files/OQMD_CaTiO3_POSCAR.txt')
-structure = pymatgen.core.Structure.from_file(args[0])
+structure = pymatgen.core.Structure.from_file('mk1/crystal_files/ICSD_HfO2_CIF.cif')
 structure_matcher = pymatgen.analysis.structure_matcher.StructureMatcher()
 structure.add_oxidation_state_by_guess()
+
+iO = []
+vac_structures = {}
+for i, site in enumerate(structure.sites):
+    if site.specie.symbol == 'O':
+        iO.append(i)
+for i in iO:
+    new_struct = structure.copy()
+    new_struct.remove_sites([i])
+    vac_structures[i] = new_struct
+
+unique_structures = {}
+
+for i, vac_structure in vac_structures.items():
+    if i == 0:
+        unique_structures[i] = vac_structure
+    n_dupl = 0
+    for j, unique_structure in unique_structures.items():
+        if structure_matcher.fit(vac_structure, unique_structure) == True:
+            n_dupl += 1
+    if n_dupl == 0:
+        unique_structures[i] = vac_structure
+
+
+struc_NN = {}
+
+for i in range(len(unique_structures.keys())):
+    pos_arg = list(unique_structures.keys())[i]
+
+    struc_NN[i] = CrystalNN(weighted_cn=True, cation_anion=True, porous_adjustment=False, \
+    distance_cutoffs=None, x_diff_weight=0).get_nn_info(structure, pos_arg)
+
+
+print(struc_NN.values())
+
