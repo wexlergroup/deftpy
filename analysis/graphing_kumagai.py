dab0c9b3e883d6efdd040886789c32416b195b16
avi-i <a.isakov@wustl.edu>
2024-02-22 13:57:50 -0600

    graphing now has an ability to sort data by number of elements and graph a bar chart/histogram. Color coding by group available for binaries. A clean version of kumagai analysis under kumagai_analysis_clean.py is available. This will exclude code attempts and checks since the code runs smoothly now. Notes are included

--- analysis/graphing_kumagai.py
+++ analysis/graphing_kumagai.py
@@ -10,29 +10,36 @@ from sklearn.model_selection import KFold, cross_val_score
 from mendeleev import element
 
 # Histogram
+'''
+df_hist = pd.read_csv("kumagai_Vr_Eb_full_frac.csv")
+df_hist["element_num"] = df_hist.formula.apply(lambda x:  int(len(Composition(x))))
+# plt.hist(df_hist["element_num"], bins=[2, 3, 4, 5], align='mid')
+# Counting the frequency of each number of elements
+element_count = df_hist['element_num'].value_counts().sort_index()
+# Plotting the counts
+plt.bar(element_count.index, element_count.values, color='skyblue')
+plt.title("distribution of the number of elements in the metal oxide compositions of the Kumagai data set", size=8)
+plt.xlabel("number of elements")
+plt.xticks([2, 3, 4, 5])
+plt.ylabel("number of metal oxides")
+plt.savefig("kumagai_full_frac_bar.png")
+plt.show()'''
+# exit(23)
 
-# df_hist = pd.read_csv("kumagai_Vr_Eb_full_frac.csv")
-# df_hist["element_num"] = df_hist.formula.apply(lambda x:  int(len(Composition(x))))
-# plt.hist(df_hist["element_num"], align='mid')
-# plt.title("distribution of elements in metal oxide compositions")
-# plt.xlabel("number of elements")
-# plt.ylabel("number of metal oxides")
-# plt.savefig("kumagai_full_frac_histogram.png")
-# plt.show()
-
-# CFM
-df_plot = pd.read_csv("kumagai_Eb_Vr_frac.csv")
+# # CFM
+df_plot = pd.read_csv("kumagai_binary_Eb_frac_Vr_from_csv.csv")
 
 # # get binaries from binaries/ternaries
-# df_plot["is_binary"] = df_plot.formula.apply(lambda x: len(Composition(x)) == 2)
+df_plot["is_binary"] = df_plot.formula.apply(lambda x: len(Composition(x)) == 2)
 
 fig, axs = plt.subplots(ncols=3, figsize=(12, 4))
 
 for i, charge in enumerate([0, 1, 2]):
     cfm = linear_model.HuberRegressor()
-    #X = df_plot.loc[df_plot.charge == charge, ["vr_max", "band_gap"]]
-    #y = df_plot.loc[df_plot.charge == charge, "vacancy_formation_energy"]
-    X = df_plot.loc[df_plot.charge == charge, ["Eb_sum", "vr_max", "band_gap"]]
+    # X = df_plot.loc[df_plot.charge == charge, ["vr_max", "band_gap"]]
+    # y = df_plot.loc[df_plot.charge == charge, "vacancy_formation_energy"]
+    # X = df_plot.loc[df_plot.charge == charge, ["Eb_sum", "vr_max", "band_gap"]]
+    X = df_plot.loc[df_plot.charge == charge, ["Eb_sum", "vr_max", "band_gap", "o2p_center_from_vbm"]]
     y = df_plot.loc[df_plot.charge == charge, "vacancy_formation_energy"]
     cfm.fit(X, y)
     y_pred = cfm.predict(X)
@@ -60,36 +67,38 @@ for i, charge in enumerate([0, 1, 2]):
     # # color_map = df_colors["element_num"].map(colors)
 
     # define colors for element groups
-    # df_colors = df_plot[df_plot["charge"] == i]
-    # periodic_table_groups = {
-    #     "H": 1, "He": 18,
-    #     "Li": 1, "Be": 2, "B": 13, "C": 14, "N": 15, "O":16, "F": 17, "Ne": 18,
-    #     "Na": 1, "Mg": 2, "Al": 13, "Si": 14, "P": 15, "S": 16, "Cl": 17, "Ar": 18,
-    #     "K": 1, "Ca": 2, "Sc": 3, "Ti": 4, "V": 5, "Cr": 6, "Mn": 7, "Fe": 8, "Co": 9, "Ni": 10, "Cu": 11, "Zn": 12,
-    #     "Ga": 13, "Ge": 14, "As": 15, "Se": 16, "Br": 17, "Kr": 18,
-    #     "Rb": 1, "Sr": 2, "Y": 3, "Zr": 4, "Nb": 5, "Mo": 6, "Tc": 7, "Ru": 8, "Rh": 9, "Pd": 10, "Ag": 11, "Cd": 12,
-    #     "In": 13, "Sn": 14, "Sb": 15, "Te": 16, "I": 17, "Xe": 18,
-    #     "Cs": 1, "Ba": 2, "La": 3, "Hf": 4, "Ta": 5, "W": 6, "Re": 7, "Os": 8, "Ir": 9, "Pt": 10, "Au": 11, "Hg": 12,
-    #     "Tl": 13, "Pb": 14, "Bi": 15, "Po": 16, "At": 17, "Rn": 18
-    # }
+    df_colors = df_plot[df_plot["charge"] == i]
+    periodic_table_groups = {
+        "H": 1, "He": 18,
+        "Li": 1, "Be": 2, "B": 13, "C": 14, "N": 15, "O":16, "F": 17, "Ne": 18,
+        "Na": 1, "Mg": 2, "Al": 13, "Si": 14, "P": 15, "S": 16, "Cl": 17, "Ar": 18,
+        "K": 1, "Ca": 2, "Sc": 3, "Ti": 4, "V": 5, "Cr": 6, "Mn": 7, "Fe": 8, "Co": 9, "Ni": 10, "Cu": 11, "Zn": 12,
+        "Ga": 13, "Ge": 14, "As": 15, "Se": 16, "Br": 17, "Kr": 18,
+        "Rb": 1, "Sr": 2, "Y": 3, "Zr": 4, "Nb": 5, "Mo": 6, "Tc": 7, "Ru": 8, "Rh": 9, "Pd": 10, "Ag": 11, "Cd": 12,
+        "In": 13, "Sn": 14, "Sb": 15, "Te": 16, "I": 17, "Xe": 18,
+        "Cs": 1, "Ba": 2, "La": 3, "Hf": 4, "Ta": 5, "W": 6, "Re": 7, "Os": 8, "Ir": 9, "Pt": 10, "Au": 11, "Hg": 12,
+        "Tl": 13, "Pb": 14, "Bi": 15, "Po": 16, "At": 17, "Rn": 18
+    }
     # color_map = {1: 'red', 2: 'blue', 3: 'green', 4: 'orange', 5: 'purple', 6: 'brown', 7: 'pink', 8: 'gray', 9: 'olive', 10: 'cyan', 11: 'magenta', 12: 'yellow', 13: 'lime', 14: 'teal', 15: 'navy', 16: 'maroon', 17: 'skyblue', 18: 'black'}
-    #
-    # def get_group(element):
-    #     if element in periodic_table_groups:
-    #         return periodic_table_groups[element]
-    #     else:
-    #         return None
-    #
-    # df_colors['Groups'] = df_colors['formula'].str.findall(r'[A-Z][a-z]*').apply(lambda x: [get_group(e) for e in x])
-    # df_colors['group'] = df_colors['Groups'].apply(lambda x: x[0])
-    # # print(df_colors['group'])
-    # colors = df_colors['group'].map(color_map)
+    # color map w/o TM
+    color_map = {1: 'red', 2: 'blue', 13: 'lime', 14: 'teal', 15: 'navy', 16: 'maroon', 17: 'skyblue', 18: 'black'}
+    def get_group(element):
+        if element in periodic_table_groups:
+            return periodic_table_groups[element]
+        else:
+            return None
+
+    df_colors['Groups'] = df_colors['formula'].str.findall(r'[A-Z][a-z]*').apply(lambda x: [get_group(e) for e in x])
+    df_colors['group'] = df_colors['Groups'].apply(lambda x: x[0])
+    print(df_colors['group'])
+    colors = df_colors['group'].map(color_map)
+
     # # print(color_map)
     # # exit(32)
 
     # Plot results
-    # axs[i].scatter(y_pred, y, c=colors)
-    axs[i].plot(y_pred, y, "o")
+    axs[i].scatter(y_pred, y, c=colors)
+    # axs[i].plot(y_pred, y, "o")
 
     # Plot parity line
     # axs[i].plot([-4, 10], [-4, 10], "--", color="black")
@@ -100,8 +109,9 @@ for i, charge in enumerate([0, 1, 2]):
     axs[i].set_ylim(-4, 10)
 
     # Add equation
-    equation = "$E_v = {:.2f} {:+.2f} E_b {:+.2f} V_r {:+.2f} E_g$".format(cfm.intercept_, cfm.coef_[0], cfm.coef_[1],
-                                                                           cfm.coef_[2])
+    # equation = "$E_v = {:.2f} {:+.2f} E_b {:+.2f} V_r {:+.2f} E_g$".format(cfm.intercept_, cfm.coef_[0], cfm.coef_[1],
+    #                                                                        cfm.coef_[2])
+    equation = "$E_v = {:.2f} {:+.2f} E_b {:+.2f} V_r {:+.2f} E_g {:+.2f} O_p$".format(cfm.intercept_, cfm.coef_[0], cfm.coef_[1], cfm.coef_[2], cfm.coef_[3])
     axs[i].set_xlabel(equation)
 
     # Add MAE
@@ -123,12 +133,14 @@ for i, charge in enumerate([0, 1, 2]):
     # Add Legend
     # legends = {"binary": "blue", "ternary": "red"}
     # axs[i].legend(colors, loc="lower right")
-    # axs[i].legend(df_colors['group'].map(color_map), loc='lower right')
 
     # Add y-axis label
     if i == 0:
         axs[i].set_ylabel("$E_v$ (eV)")
-
+    if i == 2:
+        for x in color_map:
+            axs[i].scatter([], [], c=color_map[x], label=f'group {x}')
+        axs[i].legend(loc='lower right', bbox_to_anchor=(1.5, 0.15))
 plt.tight_layout()
-plt.savefig("kumagai_ternary_vr_from_csv_eb_frac_KF5.png", dpi=300)
+plt.savefig("kumagai_binary_Eb_frac_Vr_fcsv_bygroup.png", dpi=300)
 plt.show()
---
+++ analysis/kumagai_analysis_clean.py
@@ -0,0 +1,203 @@
+import os
+import tarfile
+from glob import glob
+import pandas as pd
+from pymatgen.analysis.local_env import CrystalNN
+from crystal_analysis import Crystal
+from pymatgen.core import Structure, Composition
+import numpy as np
+from tqdm import tqdm
+from pymatgen.analysis.bond_valence import BVAnalyzer as BVA
+
+def main():
+    data_path = "../data/papers/kumagai/"
+    # Li2O test
+    with tarfile.open(glob(data_path +"oxygen_vacancies_db_data/Li2O/*0.tar.gz")[0], "r:gz") as tar:
+        # Read the member with the name "CONTCAR-finish"
+        f = tar.extractfile("CONTCAR-finish")
+        #print(f.read().decode("utf-8"))
+
+    # Get data is only necessary if you are not indexing the data and have not utilized getting_indexes.py
+    # df_0 = pd.read_csv(data_path + "vacancy_formation_energy_ml/charge0.csv")  # neutral vacancies
+    # df_1 = pd.read_csv(data_path + "vacancy_formation_energy_ml/charge1.csv")  # +1 charged vacancies
+    # df_2 = pd.read_csv(data_path + "vacancy_formation_energy_ml/charge2.csv")  # +2 charged vacancies
+
+    # Add charge column
+    # df_0["charge"] = 0
+    # df_1["charge"] = 1
+    # df_2["charge"] = 2
+
+    # Combine dataframes
+    # df = pd.concat([df_0, df_1, df_2], ignore_index=True).reset_index(drop=True)
+
+    # Remove the column named "Unnamed: 0"
+    # df = df.drop("Unnamed: 0", axis=1)
+
+    df = pd.read_csv("complete_df_indexed.csv")  # aquired using getting_indexes.py
+
+    # Remove non-binary compounds
+    df["is_binary"] = df.formula.apply(lambda x: len(Composition(x)) == 2)
+    df_binary = df.loc[df.is_binary].reset_index(drop=True)
+
+    # Remove compounds with transition metals
+    df_binary["has_transition_metal"] = df_binary.formula.apply(
+        lambda x: any([el.is_transition_metal for el in Composition(x)]))
+    df_binary = df_binary.loc[~df_binary.has_transition_metal].reset_index(drop=True)
+
+    # Remove unnecessary columns for binaries
+    df_plot = df_binary[["formula", "full_name", "band_gap", "formation_energy", "nn_ave_eleneg", "o2p_center_from_vbm",
+                         "vacancy_formation_energy", "charge", "vacancy_index"]].reset_index(drop=True)
+    # df_plot.to_csv("Kumagai_binary_clean.csv")
+
+    # **Binary/Ternary compounds + Removes compounds w/ transition metals
+    # df["is_binary_or_ternary"] = df["formula"].apply(lambda x: 2 <= len(Composition(x).elements) <= 3)
+    # df_ternary = df.loc[df.is_binary_or_ternary].reset_index(drop=True)
+    #
+    # df_ternary["has_transition_metal"] = df_ternary.formula.apply(
+    #     lambda x: any([el.is_transition_metal for el in Composition(x)]))
+    # df_ternary = df_ternary.loc[~df_ternary.has_transition_metal].reset_index(drop=True)
+
+    # ** Remove unnecessary columns for binary/ternaries
+    # df_plot = df_ternary[["formula", "full_name", "band_gap", "formation_energy", "nn_ave_eleneg", "o2p_center_from_vbm",
+    #                      "vacancy_formation_energy", "charge", "vacancy_index"]].reset_index(drop=True)
+    # df_plot.to_csv("Kumagai_ternary.csv")
+
+    # *** Remove compounds with transition metals for full set
+    # df["has_transition_metal"] = df.formula.apply(
+    #     lambda x: any([el.is_transition_metal for el in Composition(x)]))
+    # df = df.loc[~df.has_transition_metal].reset_index(drop=True)
+
+    # **** Remove unnecessary columns for full data set
+    # df_plot = df[["formula", "full_name", "band_gap", "formation_energy", "nn_ave_eleneg", "o2p_center_from_vbm",
+    #               "vacancy_formation_energy", "charge", "vacancy_index"]].reset_index(drop=True)
+    # df_plot.to_csv("Kumagai_full.csv")
+
+    # Calculate crystal reduction potentials for binaries and non-binaries - from Vr CSV files
+    vr_list = []
+    for _, row in df_plot.iterrows():
+        formula = row["formula"]
+        composition = Composition(formula)
+        metal_info = []  # For storing information about each metal in the formula
+        for el in composition.elements:
+            if el.symbol != "O":
+                metal = el.symbol
+                n_metal = composition.get_el_amt_dict()[metal]
+                try:
+                    composition = Composition.add_charges_from_oxi_state_guesses(composition)  # guess uses pymatgen get_oxi_state function
+                    oxidation_states = Composition.oxi_state_guesses(composition)
+                except ValueError:
+                    metal_info.append({
+                        "metal": metal,
+                        "n_metal": n_metal,
+                        "oxi_state": np.nan,
+                        "vr": np.nan
+                    })
+                    pass
+                try:
+                    get_dict = oxidation_states[0]
+                    oxi_state = get_dict[metal]
+                    # get vr csv as df
+                    vr_df = pd.read_csv("../data/features/Vr.csv")
+                    # match to metal
+                    vr_m_df = vr_df[vr_df["elem"] == metal]
+                    # match to oxi_state
+                    vr_m_o_df = vr_m_df[vr_m_df["n"] == oxi_state]
+                    # choose optimal vr for oxidation
+                    vr_max_df = vr_m_o_df[vr_m_o_df['m'] == vr_m_o_df['m'].max()]
+                    # print(vr_max_df)
+                    vr = vr_max_df["Vr"].iloc[0]
+                    # print(vr)
+                    # vr = n_atoms * formation_energy / n_metal / oxi_state #original hand calculation of Vr for binary
+                    metal_info.append({
+                        "metal": metal,
+                        "n_metal": n_metal,
+                        "oxi_state": oxi_state,
+                        "vr": vr
+                    })
+                except IndexError or ValueError:
+                    metal_info.append({
+                        "metal": metal,
+                        "n_metal": n_metal,
+                        "oxi_state": np.nan,
+                        "vr": np.nan
+                    })
+                    pass
+        max_vr = max(d['vr'] for d in metal_info)
+        vr_list.append(max_vr)
+    # can pre-check vr by ensuring the lengths of vr_list and the df are equal
+    # print(len(vr_list))
+    # print(len(df_plot))
+    # Calculate 'vr_max' for the entire DataFrame
+    df_plot["vr_max"] = vr_list
+
+    # Save to CSV
+    # df_plot.to_csv("kumagai_ternary_vr_from_csv.csv")
+
+    # remove any values where oxi_state could not be assigned
+    df_plot = df_plot.dropna()
+
+    # using corrected structure files
+    Eb_sum = []
+    for defect in tqdm(df_plot["vacancy_formation_energy"].unique()):
+        df_defect = df_plot[df_plot["vacancy_formation_energy"] == defect]
+        formula = df_defect["formula"].iloc[0]
+        index = df_defect["vacancy_index"].iloc[0]
+
+        with tarfile.open(glob(data_path + "site_info.tar.gz")[0], "r:gz") as tar:
+            tar.extractall(path=data_path)
+            cif_path = os.path.join(data_path, "site_info", formula, "supercell.cif")
+            if os.path.exists(cif_path):
+                # MUST use .from_file to ensure the structure is not minimized/primitive or the index will be lost
+                base_structure = Structure.from_file(cif_path)
+                if base_structure:
+                    # for generating oxidation states using Bond Valence (better way - avi)
+                    try:
+                        structure = BVA().get_oxi_state_decorated_structure(base_structure)
+
+                        # # if you want to check symmetries/indexing all of these should match cell_info.txt files
+                        # analyzer = SpacegroupAnalyzer(structure)
+                        # symmetry_dataset = analyzer.get_symmetry_dataset()
+                        #
+                        # wyckoff_symbols = symmetry_dataset['wyckoffs']
+                        # wyckoff = wyckoff_symbols[index]
+                        #
+                        # print(formula, index, f"wyckoff = {wyckoff}")
+                        # # structures.append(structure)
+
+                        # generate the crystal object - can supply nn_finder to weight cn appropriately
+                        # crystal = Crystal(pymatgen_structure=structure, n=index)
+                        crystal = Crystal(pymatgen_structure=structure, n=index, nn_finder=CrystalNN(weighted_cn=True, cation_anion=True), use_weights=True)
+
+                        CN = crystal.cn_dicts
+                        Eb = crystal.bond_dissociation_enthalpies
+
+                        # Eb_sum = [
+                        #     np.sum(np.array(list(cn.values())) * np.array(list(be.values())))
+                        #     for cn, be in zip(CN, Eb)
+                        # ]
+
+                        # print(f"Eb_sum length: {len(Eb_sum)}")
+
+                        for CN_dict, Eb_dict in zip(CN, Eb):
+                            CN_array = np.array(list(CN_dict.values()))
+                            Eb_array = np.array(list(Eb_dict.values()))
+                            Eb_sum.append(np.sum(CN_array * Eb_array))
+                            # print(CN_array, Eb_array)
+                        # print("length is " + str(len(Eb_sum)))
+
+                    except ValueError:
+                        Eb_sum.append(np.nan)
+                        pass
+                else:
+                    print(f"Error: List empty - {base_structure}")
+                    # print(f"Error: List empty - {parser_list}")
+            else:
+                print(f"Error: File not found - {cif_path}")
+    df_plot["Eb_sum"] = Eb_sum
+    df_plot = df_plot.dropna()
+    # this data frame will have Vr and Eb_sum in addition to the original columns kept for analysis
+    print('complete')
+    # df_plot.to_csv("kumagai_binary_Eb_frac_Vr_from_csv.csv")
+
+if __name__ == "__main__":
+    main()
